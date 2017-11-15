d4d900c77380098c76e7ecaad6d6ffafe226d406
Aaron Montoya <aatoya7@gmail.com>
2017-11-15 12:22:41 -0700

    Added and updated ChatFrame class.

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -5,5 +5,22 @@ import javax.swing.JFrame;
 
 public class ChatFrame extends JFrame
 {
-
+	private ChatbotController appController;
+//	private ChatPanel appPane;
+	
+	public ChatFrame(ChatbotController appController)
+	{
+		super();
+		this.appController = appController;
+//		appPane = new ChatPanel(appController);
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+//		this.setContentPane(appPane);
+		this.setTitle("Chatting with ChatPanda");
+		this.setSize(600, 600);
+		this.setVisible(true);
+	}
 }
---
+++ src/chat/view/GUIPanel.java
@@ -0,0 +1,36 @@
+package chat.view;
+
+import javax.swing.JPanel;
+import chat.controller.ChatbotController;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.SpringLayout;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUIPanel extends JPanel
+{
+	private ChatbotController appController;
+	
+	public GUIPanel(ChatbotController appController)
+	{
+		super();
+		this.appController= appController;
+	}	
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+}
