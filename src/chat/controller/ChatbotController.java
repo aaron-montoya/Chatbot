7651e090dcf80918f3716862454c23aecae1bf45
Aaron Montoya <aatoya7@gmail.com>
2017-11-15 12:40:09 -0700

    Added all the imports, data members and stubs for methods to the ChatPanel class.

--- src/chat/controller/ChatbotController.java
+++ src/chat/controller/ChatbotController.java
@@ -1,17 +1,20 @@
 package chat.controller;
 
 import chat.model.Chatbot;
+import chat.view.ChatFrame;
 import chat.view.PopupDisplay;
 
 public class ChatbotController
 {
 	private Chatbot chatbot;
 	private PopupDisplay display;
+	private ChatFrame appFrame;
 	
 	public ChatbotController()
 	{
 		chatbot = new Chatbot("Aaron Montoya");
 		display = new PopupDisplay();
+		appFrame = new ChatFrame(this);
 	}
 	
 	public void start()
@@ -20,13 +23,13 @@ public class ChatbotController
 		String response = display.getResponse("What do you want to talk about?");
 		
 		//If the response is longer than 2 and isn't quit, keep looping
-		while (chatbot.lengthChecker(response) && !chatbot.quitChecker(response))
-		{
-			//Call the popupChat method and pass the response to it
-			response = popupChat(response);
-			//Display the final response
-			response = display.getResponse(response);
-		}
+//		while (chatbot.lengthChecker(response) && !chatbot.quitChecker(response))
+//		{
+//			//Call the popupChat method and pass the response to it
+//			response = popupChat(response);
+//			//Display the final response
+//			response = display.getResponse(response);
+//		}
 	}
 	
 	//public String interactWithChatbot(String input)
---
+++ src/chat/view/ChatPanel.java
@@ -0,0 +1,39 @@
+package chat.view;
+
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.SpringLayout;
+import chat.controller.ChatbotController;
+import java.awt.Color;
+
+public class ChatPanel extends JPanel
+{
+	private ChatbotController appController;
+	private JButton chatButton;
+	private JTextField inputField;
+	private JTextArea chatArea;
+	private SpringLayout appLayout;
+	
+	public ChatPanel(ChatbotController appController)
+	{
+		super();
+		this.appController= appController;
+	}	
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+}
