01a10588568bc34749737a43497014fd989eeb41
Aaron Montoya <aatoya7@gmail.com>
2018-03-12 13:34:49 -0600

    Added sortHashMap

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -212,6 +212,14 @@ public class CTECTwitter
 			}
 		}
 	}
+	
+	private ArrayList<Map.Entry<String, Integer>> sortHashMap()
+	{
+		ArrayList<Map.Entry<String, Integer>> entries = new ArrayList<Map.Entry<String, Integer>>(wordsAndCount.entrySet());
+		entries.sort((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()));
+		
+		return entries;
+	}
 }
 
 
